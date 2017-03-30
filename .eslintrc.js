4730e3aa2f187f4ce2cb96984265b86350292ad3
Neil Anderson <njp.anderson@icloud.com>
2017-03-30 19:54:51 +0100

    convert quotes and add production global

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,41 @@
 module.exports = {
-	"extends": ["eslint:recommended", "plugin:react/recommended"],
-	"parserOptions": {
-		"ecmaFeatures": {
-			"jsx": true,
-			"experimentalObjectRestSpread": true
+	'extends': ['eslint:recommended', 'plugin:react/recommended'],
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true,
+			'experimentalObjectRestSpread': true
 		},
-		"sourceType": "module"
+		'sourceType': 'module'
 	},
-	"plugins": [
-		"react"
+	'plugins': [
+		'react'
 	],
-	"env": {
-		"browser": true,
-		"node": true,
-		"mocha": true,
-		"es6": true,
-		"jquery": true
+	'env': {
+		'browser': true,
+		'node': true,
+		'mocha': true,
+		'es6': true,
+		'jquery': true
 	},
-	"rules": {
+	'rules': {
 		// enable additional rules
-		"indent": ["error", "tab"],
-		"linebreak-style": ["error", "unix"],
-		"quotes": ["error", "single"],
-		"semi": ["error", "always"],
+		'indent': ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		'quotes': ['error', 'single'],
+		'semi': ['error', 'always'],
 
 		// override default options for rules from base configurations
-		//"comma-dangle": ["error", "always"],
-		//"no-cond-assign": ["error", "except-parens"],
+		//'comma-dangle': ['error', 'always'],
+		//'no-cond-assign': ['error', 'except-parens'],
 
 		// disable rules from base configurations
-		"no-console": "off",
+		'no-console': 'off',
 
 		// react specific rules
-		"react/no-danger": "off",
-		"react/display-name": "off"
+		'react/no-danger': 'off',
+		'react/display-name': 'off'
+	},
+	globals: {
+		'PRODUCTION': true
 	}
-}
+};
